99f0d6f54b34e05b20f007a4227f305019edb8ce
nijil7511 <nijil.mv@gmail.com>
2026-02-17 14:34:25 +0530

    Update test_gitclone.py

--- test_gitclone.py
+++ test_gitclone.py
@@ -29,11 +29,11 @@ def test_properlogin_validation(driver):
     try:
         error_message = WebDriverWait(driver, 5).until(EC.visibility_of_element_located((By.XPATH, ".//h1[contains(.,'Logged In Successfully')]")))
 
-        print("Pass – Logged In Sucessfully!!")
+        print("Pass – Logged In Sucessfully!")
         assert True
 
     except:
-        print("Unable to Log In. Test Failed!!")
+        print("Unable to Log In. Test Failed!")
         assert False 
     time.sleep(10)
     driver.quit()
